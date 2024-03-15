918bac1bc060ec97aeb715f0a79c05b0e8ed5cd4
Federico De Felici <fdf8210@gmail.com>
2024-03-15 17:20:04 +0100

    refactor to window class

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,35 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
+#include <string>
 #include <format>
+#include "Window.h"
 
 int main() {
 
-	if (glfwInit() == 0) 
-	{
-		std::cout << "Error glfw init\n";
-		return -1;
-	}
+	Window Window{800, 600, "Hello OpenGL"};
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
-
-	int width = 800;
-	int height = 600;
-	const std::string title = "HelloOpenGL";
-	GLFWmonitor* monitor = NULL;
-
-	GLFWwindow* window = glfwCreateWindow(width, height, title.c_str(), monitor, NULL);
-	if (window == NULL) {
-		std::cout << "Error creating window\n";
-		return -1;
-	}
-
-	glfwMakeContextCurrent(window);
-
-	if (gladLoadGL() == 0) 
-	{
-		std::cout << "Error loading GL Functions\n";
-		return -1;
-	}
-
+	/* PrintInfo()
 	std::cout << "GL VERSION: " << glGetString(GL_VERSION) << "\n";
 	std::cout << "GL RENDERER: " << glGetString(GL_RENDERER) << "\n";
 	std::cout << "GL VENDOR: " << glGetString(GL_VENDOR) << "\n";
 	std::cout << "GLSL VERSION: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << "\n";
-
-	float lastTime = glfwGetTime();
-	float deltaTime = 0.f;
+	*/
 
 	float titleUpdateMaxTime = 1.f;
 	float titleUpdateElapsed = 0.f;
 
-	while(!glfwWindowShouldClose(window))
-	{
-		float currTime = glfwGetTime();
-		deltaTime = currTime - lastTime;
-		lastTime = currTime;
-		
-		titleUpdateElapsed += deltaTime;
-
+	while(Window.IsOpened()) 
+	{	
+		float DeltaTime = Window.DeltaTime();
+		titleUpdateElapsed += DeltaTime;
 		if (titleUpdateElapsed >= titleUpdateMaxTime) //1.1 >= 1
 		{
-			int fps = (int)(1.f / deltaTime);
-			std::string formattedTitle = std::format("OpenGL App | DeltaTime: {} - FPS: {}", deltaTime, fps);
-			glfwSetWindowTitle(window, formattedTitle.c_str());
+			int fps = (int)(1.f / DeltaTime);
+			std::string formattedTitle = std::format("OpenGL App | DeltaTime: {} - FPS: {}", DeltaTime, fps);
+			Window.SetTitle(formattedTitle);
 
 			titleUpdateElapsed -= titleUpdateMaxTime; //0.1
 		}
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+		Window.Update();
 	}
-
-	glfwDestroyWindow(window);
 	return 0;	
 }
